d7559ff026b4ebb51e3aae06be5231a8e35b0c6f
hayeon9826 <hayeon226@likelion.org>
2023-06-01 03:00:47 +0900

    feat: tailwind 컴포넌트 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,33 +1,9 @@
 import Layout from '@/components/Layout';
-import Link from 'next/link';
 
 export default function Home() {
   return (
     <Layout>
-      <div>Hello World</div>
-      <ul>
-        <li>
-          <Link href="/users/login">login</Link>
-        </li>
-        <li>
-          <Link href="/users/signin">signin</Link>
-        </li>
-        <li>
-          <Link href="/users/mypage">mypage</Link>
-        </li>
-        <li>
-          <Link href="/shops">shops</Link>
-        </li>
-        <li>
-          <Link href="/shops">shops new</Link>
-        </li>
-        <li>
-          <Link href="/shops/1/edit">shop edit</Link>
-        </li>
-        <li>
-          <Link href="/shops/1">shop detail</Link>
-        </li>
-      </ul>
+      <div>map</div>
     </Layout>
   );
 }
