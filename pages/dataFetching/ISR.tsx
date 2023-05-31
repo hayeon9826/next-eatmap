67849b48998ce1333dd9b5fa3bd47847f3139c37
hayeon9826 <hayeon226@likelion.org>
2023-06-01 00:27:14 +0900

    feat: data fetching 실습

---
+++ pages/dataFetching/ISR.tsx
@@ -0,0 +1,22 @@
+// https://nextjs.org/docs/pages/building-your-application/data-fetching/incremental-static-regeneration
+
+import type { InferGetStaticPropsType, GetStaticProps } from 'next';
+
+type Repo = {
+  name: string;
+  stargazers_count: number;
+};
+
+export const getStaticProps: GetStaticProps<{
+  repo: Repo;
+}> = async () => {
+  const res = await fetch('https://api.github.com/repos/vercel/next.js');
+  const repo = await res.json();
+  return { props: { repo }, revalidate: 5 };
+};
+
+export default function Page({
+  repo,
+}: InferGetStaticPropsType<typeof getStaticProps>) {
+  return repo.stargazers_count;
+}
